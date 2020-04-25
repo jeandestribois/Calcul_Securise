c6c3f192fa92673c8c3f0574236105aa692fd439
Jean Destribois <jean.destribois@gmail.com>
2020-04-25 18:12:56 +0200

    jean: Programme fini. push pour l'ajout du compte rendu

--- programme/src/main.c
+++ programme/src/main.c
@@ -5,7 +5,11 @@ int main(int argc, char const *argv[]) {
 
 	struct attaque attaque = initialisation_attaque();
 
-	uint64_t K = recherche_K(attaque, recherche_K_16(attaque));
+	uint64_t K_16 = recherche_K_16(attaque);
+
+	printf("K_16 = %lx\n", K_16);
+
+	uint64_t K = recherche_K(attaque, K_16);
 
 	printf("K = %lx\n", K);
 
